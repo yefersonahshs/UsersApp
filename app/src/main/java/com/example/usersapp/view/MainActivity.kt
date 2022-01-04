158590b790e6991b56e088a7eb148bb74167b76d
Yeferson Naranjo <54407133+yefersonahshs@users.noreply.github.com>
2022-01-04 04:48:42 -0500

    ajuste detail activity

--- app/src/main/java/com/example/usersapp/view/MainActivity.kt
+++ app/src/main/java/com/example/usersapp/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.usersapp.view
 
 import AppDatabase
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,6 +12,7 @@ import com.example.usersapp.R
 import com.example.usersapp.api.ApiService
 import com.example.usersapp.client.RetrofitClient
 import com.example.usersapp.models.User
+import com.example.usersapp.util.RecyclerItemClickListener
 import com.example.usersapp.util.UserAdapter
 import retrofit2.Call
 import retrofit2.Callback
@@ -28,8 +30,6 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
 
 
         val call = RetrofitClient()
@@ -53,6 +53,8 @@ class MainActivity : AppCompatActivity(){
 
 
         })
+
+
     }
 
 /*
@@ -67,14 +69,25 @@ class MainActivity : AppCompatActivity(){
 
 
        // room.UserDao().insert(users)
+//    val adapter= KotlinRecyclerAdapter(applicationContext,arrayList)
 
-        rv_user_list.apply {
+    rv_user_list.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter =
                 UserAdapter(
                     users
                 )
         }
+
+
+            rv_user_list.addOnItemTouchListener(
+                RecyclerItemClickListener(applicationContext,
+                    RecyclerItemClickListener.OnItemClickListener { view, position ->
+                        val intent= Intent(applicationContext,DetailActivity::class.java)
+                        intent.putExtra("user", users[position].id)
+                        startActivity(intent)
+                    })
+            )
 
 
 
